2f07f178c56885fbe89499223d2a61e477aa16c8
uditdhankar23 <113027856+uditdhankar23@users.noreply.github.com>
2023-08-08 02:53:35 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,43 @@
+import cv2
+cap = cv2.VideoCapture(0)
+import mediapipe as mp
+import pyautogui
+# moving mouse
+hand_detector = mp.solutions.hands.Hands()
+drawing_utils = mp.solutions.drawing_utils
+screen_width , screen_height = pyautogui.size()
+
+index_y = 0
+while True:
+    _, frame = cap.read()
+    frame = cv2.flip(frame,1)                   # flip frame on y axis 1--> y
+    frame_heigth , frame_width , _ = frame.shape
+    rgb_frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+    output = hand_detector.process(rgb_frame)
+    hands = output.multi_hand_landmarks
+    if hands:
+        for hand in hands:
+            drawing_utils.draw_landmarks(frame,hand)
+            landmarks = hand.landmark
+            for id,landmark in enumerate(landmarks):
+                x = int(landmark.x*frame_width)
+                y = int(landmark.y*frame_heigth)
+
+                # want the index finger landmark only to control the mouse
+                if id==8:
+                    cv2.circle(img =frame, center=(x,y), radius = 10, color = (0,255,255))
+                    index_x = screen_width/frame_width * x
+                    index_y = screen_height/frame_heigth*y
+
+                    pyautogui.moveTo(index_x,index_y)
+
+                if id==4:
+                    cv2.circle(img =frame, center=(x,y), radius = 10, color = (0,255,255))
+                    thumb_x = screen_width/frame_width * x
+                    thumb_y = screen_height/frame_heigth*y
+                    print('outiside',abs(index_y-thumb_y))
+                    if abs(index_y-thumb_y)<20:
+                        pyautogui.click()
+                        pyautogui.sleep(1)
+    cv2.imshow('teddy',frame)
+    cv2.waitKey(1)
